be94bf976de2c67067e802c4a647dfa80b326489
Xiaoling-Wei <wei1241417782@gmail.com>
2026-02-25 11:07:20 -0800

    Clean up IArticleService documentation
    Removed unnecessary comments and documentation from IArticleService interface.

--- main/java/com/manage/springboot/service/IArticleService.java
+++ main/java/com/manage/springboot/service/IArticleService.java
@@ -3,14 +3,7 @@ package com.manage.springboot.service;
 import com.manage.springboot.entity.Article;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author zsc
- * @since 2022-03-22
- */
+
 public interface IArticleService extends IService<Article> {
 
 }
