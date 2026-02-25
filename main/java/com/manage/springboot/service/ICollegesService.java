342d5ed69d9b6e84a74dedf8dd4d9a5a54a26918
Xiaoling-Wei <wei1241417782@gmail.com>
2026-02-25 11:07:32 -0800

    Clean up ICollegesService interface documentation
    Removed unnecessary comments and documentation from ICollegesService interface.

--- main/java/com/manage/springboot/service/ICollegesService.java
+++ main/java/com/manage/springboot/service/ICollegesService.java
@@ -3,14 +3,7 @@ package com.manage.springboot.service;
 import com.manage.springboot.entity.Colleges;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- * 院校 服务类
- * </p>
- *
- * @author zsc
- * @since 2023-04-07
- */
+
 public interface ICollegesService extends IService<Colleges> {
 
 }
